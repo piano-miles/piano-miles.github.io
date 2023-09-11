139397b476f5ddded461c2067486a81558f36f1c
Miles Waugh <miles.m.waugh@gmail.com>
2023-09-11 10:48:46 -0700

    Update main.js

--- t/main.js
+++ t/main.js
@@ -81,13 +81,16 @@ for (let i = 0; i < links.length; i++) {
     pairs[key] = 'https://piano-miles.github.io/' + link
 }
 
-if ((params = getShortLink()).length) { // If there are params
-    if (params.length > 1) // If there are multiple params
-        for (let i = 1; i < params.length; i++) // For each param after the first
-            pairs[params[i]] !== undefined && // If the link exists
-                window.open(pairs[params[i]], '_blank') // Open in a new tab
+if ((params = getShortLink()).length) {
+  if (params.length > 1) {
+    for (let i = 1; i < params.length; i++) // For each param after the first
+        pairs[params[i]] !== undefined && // If the link exists
+            window.open(pairs[params[i]], '_blank') // Open in a new tab
+  }
 
-    pairs[params[0]] !== undefined // If the primary link exists
-        ? window.location.replace(pairs[params[0]], '_blank') // Redirect to the pimary link
-        : addDescription() // Otherwise add the description
-} else addDescription() // If there are no params, add the description
+  pairs[params[0]] !== undefined // If the primary link exists
+      ? window.location.replace(pairs[params[0]], '_blank') // Redirect to the pimary link
+      : addDescription() // Otherwise add the description
+} else {
+  addDescription() // If there are no params, add the description
+}
